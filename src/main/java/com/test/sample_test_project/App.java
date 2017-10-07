3210bb4dccbe6de92282968319dec8cec5bcf786
vamshi4040 <upender785@gmail.com>
2017-10-07 06:51:24 +0530

    initial commit

---
+++ src/main/java/com/test/sample_test_project/App.java
@@ -0,0 +1,13 @@
+package com.test.sample_test_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
